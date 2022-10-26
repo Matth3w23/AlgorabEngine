9c4d29a920051b00e61c16163666360aa1f6c16c
Matthew Hale <Matth3w4000@gmail.com>
2022-10-27 00:59:39 +0100

    Moved some files, worked on camera class

--- Algorab/src/camera.h
+++ Algorab/src/camera.h
@@ -1,20 +1,47 @@
 #pragma once
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
-glm::vec3 universeUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
 
 class Camera {
 private:
-	glm::vec3 cameraPos;
+	//all in world space, which is left handed (y up)
+	glm::vec3 position;
 	glm::vec3 forward;
 	glm::vec3 right;
 	glm::vec3 up;
 
+	glm::vec3 universeUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+	float fieldOfView = 60.0f;
+	float aspectRatio = 800.0f/600.0f;
+	float nearPlane = 0.1f;
+	float farPlane = 100.0f;
+
+	glm::mat4 viewMatrix;
+	glm::mat4 projectionMatrix;
+
 public:
 	Camera();
 	~Camera();
-	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 lookingAt = glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3 upVector = glm::vec3(0.0f, 1.0f, 0.0f)); //initialise with start position and looking towards lookingAt
-	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 forwardVector = glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3 upVector = glm::vec3(0.0f, 1.0f, 0.0f)); //initialise with position and direction vectors
+	Camera(glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 directionIndicator = glm::vec3(0.0f, 0.0f, 1.0f), bool lookAt = false);
 
-	//TODO: test
+	void updateViewMatrix();
+	void updateProjectionMatrix();
+
+	void lookAt(glm::vec3 position);
+
+	float getFov();
+	float getAspectRatio();
+	float getNearPlane();
+	float getFarPlane();
+
+	void setFieldOfView(float fov);
+	void setAspectRatio(float aspect);
+	void setNearPlane(float near);
+	void setFarPlane(float far);
+
+	void setProjectionFrustum(float fov, float aspect, float near, float far, bool updateProjection = true);
+
 };
